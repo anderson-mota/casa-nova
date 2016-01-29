01cec79fd60feb519036d61ba9e4cf4b2819f308
Anderson Mota <anderson.mota12@gmail.com>
2016-01-29 22:43:19 +0000

    Added form contact in one page site.

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,7 @@ angular.module('myApp', [
     config(['$routeProvider', function ($routeProvider) {
         var theme = "theme/infusion/";
 
-        $routeProvider.when('/', {templateUrl: theme + 'index.html', controller: 'Main'});
+        $routeProvider.when('/', {templateUrl: 'home.html', controller: 'Main'});
         $routeProvider.when('/o-que-fazemos', {templateUrl: theme + 'what-we-do.html', controller: 'WhatWeDo'});
         $routeProvider.when('/contato', {templateUrl: theme + 'contact-us.html', controller: 'Contacts'});
         $routeProvider.when('/404', {templateUrl: theme + '404.html'});
